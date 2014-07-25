92ecf2e19bf048ab80c33e680c1d08de74d1588c
jjduke11 <TJHu@rhbs-mac-mini-7.ucsf.edu>
2014-07-25 11:43:35 -0700

    Switching from mean to inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are used to Cache the Inverse of a Matrix to reduce
+## costly computation by cutting the need to compute it repeatedly
 
-## Write a short comment describing this function
+## The first function is to create a list containing a function to set the
+## value of the vector, get the value of the vector, set the value of the
+## inverse and get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-test
 
-## Write a short comment describing this function
+
+## The second function checks to see if the inverse has been calculated.
+## If so, it will return the inverse. If not it will calculate the inverse
+## and sets the inverse in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
